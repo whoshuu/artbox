b33051d3af1c3df3ad65df8b1190d4c0876cc34d
Huu Nguyen <whoshuu@gmail.com>
2013-12-11 00:01:21 -0800

    Support for animations and angles

--- src/com/whoshuu/artbox/system/SpriteRenderSystem.java
+++ src/com/whoshuu/artbox/system/SpriteRenderSystem.java
@@ -1,38 +1,52 @@
 package com.whoshuu.artbox.system;
 
 import android.graphics.Bitmap;
+import android.graphics.Rect;
 
 import com.whoshuu.artbox.artemis.ComponentMapper;
 import com.whoshuu.artbox.artemis.Entity;
-import com.whoshuu.artbox.component.BitmapComponent;
+import com.whoshuu.artbox.component.SpriteComponent;
 import com.whoshuu.artbox.component.PositionComponent;
+import com.whoshuu.artbox.component.RenderComponent;
+import com.whoshuu.artbox.util.SizeUtil;
 
 public class SpriteRenderSystem extends RenderSystem {
 
     @SuppressWarnings("unchecked")
     public SpriteRenderSystem() {
-        super(PositionComponent.class, BitmapComponent.class);
+        super(PositionComponent.class, SpriteComponent.class, RenderComponent.class);
     }
 
     @Override
     protected void initialize() {
+        dest = new Rect();
         this.positions = new ComponentMapper<PositionComponent>(PositionComponent.class,
-                this.world);
-        this.bitmaps = new ComponentMapper<BitmapComponent>(BitmapComponent.class, this.world);
+        this.world);
+        this.bitmaps = new ComponentMapper<SpriteComponent>(SpriteComponent.class, this.world);
+        super.initialize();
     }
 
     @Override
     protected void processEntity(Entity entity) {
         PositionComponent position = positions.get(entity);
-        Bitmap bitmap = bitmaps.get(entity).getBitmap();
+        SpriteComponent bitmapComponent = bitmaps.get(entity);
+        Bitmap bitmap = bitmapComponent.getBitmap();
+        Rect source = bitmapComponent.getSource();
 
-        float x = getRenderX(position.getX());
-        float y = getRenderY(position.getY());
-        Bitmap bm = Bitmap.createScaledBitmap(bitmap, 50, 50, false);
+        float x = SizeUtil.getRenderX(position.getX());
+        float y = SizeUtil.getRenderY(position.getY());
 
-        canvas.drawBitmap(bm, x - (bm.getWidth() / 2), y - (bm.getHeight() / 2), null);
+        dest.set((int) x - (source.width() / 2),
+                (int) y - (source.height() / 2),
+                (int) x + (source.width() / 2),
+                (int) y + (source.height() / 2));
+
+        canvas.rotate((float) - Math.toDegrees(position.getAngle()), x, y);
+        canvas.drawBitmap(bitmap, source, dest, null); 
+        canvas.rotate((float) Math.toDegrees(position.getAngle()), x, y);
     }
 
+    private Rect dest;
     private ComponentMapper<PositionComponent> positions;
-    private ComponentMapper<BitmapComponent> bitmaps;
+    private ComponentMapper<SpriteComponent> bitmaps;
 }
